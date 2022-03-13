89d80dd76afd4dbbfec6ddb1c0bec30eb518fc27
Yoshiyuki Iwasaki <armada3524@gmail.com>
2022-03-13 22:35:16 +0900

    fix type file.

--- src/components/molucules/Modal/index.tsx
+++ src/components/molucules/Modal/index.tsx
@@ -1,13 +1,9 @@
 import React from "react";
 import { Content, Overlay } from "./style";
 import TaskInput from "../TaskInput";
+import { ModalType } from "./type";
 
-type Props = {
-  openFlag: boolean;
-  handleClose: any;
-};
-
-const Modal: React.FC<Props> = ({ openFlag, handleClose }) => {
+const Modal: React.FC<ModalType> = ({ openFlag, handleClose }) => {
   return (
     <>
       {openFlag && (
---
+++ src/components/molucules/Modal/type.ts
@@ -0,0 +1,4 @@
+export type ModalType = {
+  openFlag: boolean;
+  handleClose: any;
+};

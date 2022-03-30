7382301deaeb1e0334070ef4ef805d7908995521
Yoshiyuki Iwasaki <armada3524@gmail.com>
2022-03-31 01:12:46 +0900

    fix button component.

--- src/components/atoms/button/index.tsx
+++ src/components/atoms/button/index.tsx
@@ -1,8 +1,8 @@
-import React from "react";
+import { FC } from "react";
 import { ButtonType } from "./type";
 import Presenter from "./presenter";
 
-const Button: React.FC<ButtonType> = ({ children, onClick }) => {
+const Button: FC<ButtonType> = ({ children, onClick }) => {
   return <Presenter onClick={onClick}>{children}</Presenter>;
 };
 
--- src/components/atoms/button/style.tsx
+++ src/components/atoms/button/style.tsx
@@ -1,3 +1,7 @@
 import styled from "styled-components";
 
-export const ButtonLayout = styled.button``;
+export const ButtonLayout = styled.button`
+  background: #333;
+  padding: 5px;
+  color: #fff;
+`;
